5837ab706079a0f38abeccfec63b35b20cc91d7f
gavrilka <35428739+gavrilka@users.noreply.github.com>
2020-10-10 10:33:09 +0300

    Help and bot_commands update
    Updated help and bot_commands according to last changes

--- handlers/users/help.py
+++ handlers/users/help.py
@@ -11,6 +11,9 @@ async def bot_help(message: types.Message):
     text = [
         'Список команд: ',
         '/start - Начать диалог',
-        '/help - Получить справку'
+        '/email - Записать email пользователя в базу',
+        '/help - Получить справку',
+        '/weather - Узнать погоду с помощью сервиса OpenWeatherMap',
+        '/fixer - Узнать курс обмена валюты c помощью сервиса fixer.io'
     ]
     await message.answer('\n'.join(text))
--- utils/set_bot_commands.py
+++ utils/set_bot_commands.py
@@ -6,4 +6,6 @@ async def set_default_commands(dp):
         types.BotCommand("start", "Запустить бота"),
         types.BotCommand("help", "Помощь"),
         types.BotCommand("email", "Записать email пользователя "),
+        types.BotCommand("weather", "Узнать погоду"),
+        types.BotCommand("fixer", "Узнать курс обмена валюты"),
     ])
